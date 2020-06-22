f4beec489be06182dce1453f4719a13473e23250
Koushik Reddy <koushikreddy323@gmail.com>
2020-06-22 10:29:37 +0530

    selection sort  added

--- demo.py
+++ demo.py
@@ -1,2 +1,17 @@
-print('Hello')
-print('welcome to Github')
+#selection sort
+def sort(nums):
+    for i in range(5):
+        minpos=i
+        for j in range(i,6):
+            if nums[j]<nums[minpos]:
+                minpos=j
+        temp=nums[i]
+        nums[i]=nums[minpos]
+        nums[minpos]=temp
+    print(nums)
+
+
+
+nums=[5,3,6,25,18,21]
+sort(nums)
+print(nums)
