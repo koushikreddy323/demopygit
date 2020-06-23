560ac1db2a61cd1582a7cd859f24974a40c4cf8e
Koushik Reddy <koushikreddy323@gmail.com>
2020-06-23 20:07:47 +0530

    Notes Updated

--- demo.py
+++ demo.py
@@ -1,17 +1,10 @@
-#selection sort
-def sort(nums):
-    for i in range(5):
-        minpos=i
-        for j in range(i,6):
-            if nums[j]<nums[minpos]:
-                minpos=j
-        temp=nums[i]
-        nums[i]=nums[minpos]
-        nums[minpos]=temp
-    print(nums)
-
-
-
-nums=[5,3,6,25,18,21]
-sort(nums)
-print(nums)
+from tkinter import *
+box=Tk()
+txt1=Label(box,text="GUI created succesfully", font=("courier",20))
+txt1.grid(column=1,row=0)
+bt1=Button(box,text='Button 1',font=("courier",15),bg='Blue',fg='white')
+bt1.grid(column=1,row=2)
+bt2=Button(box,text='Button 2',font=("courier",15),bg='Blue',fg='white')
+bt2.grid(column=1,row=4)
+box.geometry('450x200')
+box.mainloop()
